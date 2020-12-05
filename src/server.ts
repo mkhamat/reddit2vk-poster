bce4960d620f7adddbd4d95d5fc622195cb69039
Mkhamat <myxa@xakep.org>
2020-12-05 18:04:06 +0300

    move to src

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv"
+import getPost from "./methods/getPost"
+import uploadMedia from "./methods/uploadMedia"
+import postToWall from "./methods/postToWall"
+import removeTemp from "./utils/removeTemp"
+
+dotenv.config()
+
+async function main() {
+  await getPost()
+    .then((post: any) => {
+      return uploadMedia(post)
+    })
+    .then((data: any) => {
+      return postToWall(data)
+    })
+    .then((posted) => {
+      if (posted) console.log("Succeffuly posted.")
+      removeTemp()
+    })
+}
+
+setInterval(async () => {
+  await main()
+}, 1000 * 10)
--- src/utils/removeTemp.ts
+++ src/utils/removeTemp.ts
@@ -1,6 +1,6 @@
 import fs from "fs"
 export default async function removeTemp() {
-  console.log("Cleaning up \n ___________")
+  console.log("Cleaning up")
 
   let temp = fs.readdirSync("temp")
   for (let file of temp) {
