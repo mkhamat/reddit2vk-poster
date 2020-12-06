653fcc574e6007a5680856a03daa74b680f15fe2
Mkhamat <myxa@xakep.org>
2020-12-06 15:33:58 +0300

    update test

--- src/utils/downloadFile.test.ts
+++ src/utils/downloadFile.test.ts
@@ -6,9 +6,12 @@ test("Download file", () => {
   downloadFile(
     "https://v.redd.it/x2gooh5n93351/DASH_1080?source=fallback",
     "temp/test"
-  ).then((res) => {
-    expect(fs.readdirSync("temp").indexOf("test")).toBeGreaterThanOrEqual(0)
-    expect(res).toBe(true)
-  })
-  removeTemp()
+  )
+    .then((res) => {
+      expect(res).toBe(true)
+      expect(fs.readdirSync("temp").indexOf("test")).toBeGreaterThanOrEqual(0)
+    })
+    .then(() => {
+      removeTemp()
+    })
 })
