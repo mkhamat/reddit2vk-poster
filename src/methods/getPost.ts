e45eab83f3f6591d0d64bd0e7d09b2cc2c452e18
Mkhamat <myxa@xakep.org>
2020-12-08 16:24:47 +0300

    token no longer needed for gathering posts

--- src/methods/getPost.ts
+++ src/methods/getPost.ts
@@ -13,30 +13,14 @@ form.append("grant_type", "password")
 form.append("username", username)
 form.append("password", password)
 /**
- * Get token
- * Prepare request data
  * Fetch post from reddit
  */
-export default async function getPost() {
+export default async function getPost(url: string) {
   console.log("Fetching post from reddit...")
-
-  let token = await axios
-    .post("https://www.reddit.com/api/v1/access_token", form, {
-      headers: form.getHeaders(),
-      auth: {
-        username: client_id,
-        password: app_secret,
-      },
-    })
-    .then((res) => res.data.access_token)
-  let result = await axios
-    .get("https://oauth.reddit.com/random", {
-      headers: { Authorization: `bearer ${token}` },
-    })
-    .catch((error) => {
-      console.error(error)
-      return error
-    })
+  let result = await axios.get(url).catch((error) => {
+    console.error(error)
+    return error
+  })
 
   return result.data[0].data.children[0].data
 }
